2fdf62217b92ba2f906acdd81f46a34ab50a93e5
Daniel Sendzik <daniel@sendzik.eu>
2019-05-11 14:19:57 +0200

    Modified setup file

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
+import pathlib
 from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name='gender-determinator',
     description='Determines the gender of german nouns and provides some convenient methods',
+    long_description=README,
+    long_description_content_type="text/markdown",
     version='0.1',
     license='LGPL',
     author='Daniel Sendzik',
@@ -12,3 +21,4 @@ setup(
     packages=['genderdeterminator'],
     package_data={'genderdeterminator': ['words.csv']}
 )
+
